a73637f2017ca95941322fd88afe2a60c62e399e
haresh-sai06 <shareshsainaath@gmail.com>
2025-10-07 09:19:20 +0530

    feat: new component LifeCycleA

---
+++ first/src/components/error_boundary/LifecycleA.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class LifecycleA extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         name: 'Haresh'
+      }
+      console.log('LifecycleA constructor')
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('LifecycleA getDerivedStateFromProps')
+        return null
+    }
+
+    componentDidMount() {
+        console.log('LifecycleA componentDidMount')
+    }
+  render() {
+    return (
+      console.log('LifecycleA render'),
+      <div>LifecycleA</div>
+    )
+  }
+}
+
+export default LifecycleA
